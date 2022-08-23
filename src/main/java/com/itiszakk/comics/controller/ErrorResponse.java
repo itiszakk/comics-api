18366d784642c5fbc27d6e7fef90a2542c14513e
Zakhar Kalachev <zackfnx@yandex.ru>
2022-08-23 15:59:35 +0300

    Error response object for GlobalExceptionHandler

---
+++ src/main/java/com/itiszakk/comics/controller/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.itiszakk.comics.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private final int code;
+    private final String type;
+    private final String message;
+
+    public ErrorResponse(HttpStatus httpStatus, String message) {
+        this.code = httpStatus.value();
+        this.type = httpStatus.getReasonPhrase();
+        this.message = message;
+    }
+}
